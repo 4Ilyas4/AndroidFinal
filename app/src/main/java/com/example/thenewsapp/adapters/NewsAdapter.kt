3f3209b7447858516836ef08a45ae4c667997186
Yerasyil Omirzhanov <65915633+Pingwinnel@users.noreply.github.com>
2024-05-17 17:37:31 +0500

    Update NewsAdapter.kt

--- app/src/main/java/com/example/thenewsapp/adapters/NewsAdapter.kt
+++ app/src/main/java/com/example/thenewsapp/adapters/NewsAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.thenewsapp.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,32 +21,34 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
         val articleDescriptor: TextView = itemView.findViewById(R.id.articleDescription)
         val articleDatetime: TextView = itemView.findViewById(R.id.articleDateTime)
     }
+
     private val differCallback = object : DiffUtil.ItemCallback<Article>() {
         override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
-            return oldItem.id == newItem.id && oldItem.id != null
+            return oldItem.url == newItem.url
         }
         override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
             return oldItem == newItem
         }
     }
     val differ = AsyncListDiffer(this, differCallback)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.item_news,
-            parent,
-            false
-        )
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
         return ArticleViewHolder(view)
     }
-    override fun getItemCount(): Int {
-        return differ.currentList.size
-    }
+
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
         val article = differ.currentList[position]
+
         holder.apply {
-            // Using a placeholder image
-            Glide.with(itemView)
-                .load(article.urlToImage)
+            val imageUrl = if (!article.urlToImage.isNullOrEmpty()) {
+                article.urlToImage
+            } else {
+                "https://avatars.mds.yandex.net/i?id=eae10f04989cd397301a18c9c3b748cb_l-7757016-images-thumbs&n=13"
+            }
+
+            Glide.with(itemView.context)
+                .load(imageUrl)
                 .into(articleImage)
 
             articleSource.text = article.source.name
@@ -59,8 +62,12 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
         }
     }
 
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
 
     private var onItemClickListener: ((Article) -> Unit)? = null
+
     fun setOnItemClickListener(listener: (Article) -> Unit) {
         onItemClickListener = listener
     }
